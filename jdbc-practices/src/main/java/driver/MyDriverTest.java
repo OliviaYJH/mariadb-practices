ad115e693260fc7359b2df4d53e0d99dfba180de
YuJeongHyun <0703olivia@naver.com>
2024-12-13 10:15:49 +0900

    Driver

--- jdbc-practices/src/main/java/driver/MyDriverTest.java
+++ jdbc-practices/src/main/java/driver/MyDriverTest.java
@@ -1,23 +1,23 @@
-package example;
+package driver;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionEx {
+public class MyDriverTest {
 
 	public static void main(String[] args) {
 		Connection conn = null;
 
 		try {
 			// 1. JDBC Driver 로딩
-			Class.forName("org.mariadb.jdbc.Driver"); // driver 로딩
+			Class.forName("driver.MyDriver"); // driver 로딩
 
 			// 2. 연결하기
-			String url = "jdbc:mariadb://192.168.0.19:3306/webdb";
-			conn = DriverManager.getConnection(url, "webdb", "webdb"); // (url, username, pw)
+			 String url = "jdbc:mydb://127.0.0.1:1234/webdb";
+			 conn = DriverManager.getConnection(url, "webdb", "webdb");
 
-			System.out.println("연결성공!!!");
+			 System.out.println("연결성공!!!");
 
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패: " + e);
@@ -33,6 +33,7 @@ public class ConnectionEx {
 			}
 
 		}
+
 
 	}
 

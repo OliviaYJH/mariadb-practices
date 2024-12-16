4768ec0c46091531eecd97a0615f2b5f8f30c310
YuJeongHyun <0703olivia@naver.com>
2024-12-16 11:22:48 +0900

    BookShop

--- jdbc-practices/src/main/java/bookmall/dao/UserDao.java
+++ jdbc-practices/src/main/java/bookmall/dao/UserDao.java
@@ -114,7 +114,7 @@ public class UserDao {
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
 
-			String url = "jdbc:mariadb://192.168.0.32:3306/bookmall";
+			String url = "jdbc:mariadb://192.168.0.19:3306/bookmall";
 			conn = DriverManager.getConnection(url, "bookmall", "bookmall");
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패: " + e);
